55400ea437ce2005f62f7033fc0e0622c2865d0a
Erik Balonier <erikibali@gmail.com>
2015-03-17 20:34:14 -0500

    basic structure

---
+++ index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href="css/stylesheet.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<header  class="wrap">
+			<nav>
+				<ul>
+					<li id="logo">
+					</li>
+					<li>
+						<a href="#"></a>
+					</li>
+					<li>
+						<a href="#"></a>
+					</li>
+					<li>
+						<a href="#"></a>
+					</li>
+					<li>
+						<a href="#"></a>
+					</li>
+				</ul>
+			</nav>
+			<section>
+				<h1>
+
+				</h1>
+				<h2>
+
+				</h2>
+				<a href="#"></a>
+				<span> </span>
+				<a href=""> </a>
+				<div>
+					<img src="">
+					<img src="">
+					<img src="">
+					<img src="">
+				</div>
+			</section>					
+		</header>
+		<main class="wrap">
+			<h2> </h2>
+			<figure>
+				<span id="img1"></span>
+				<figcaption>
+				</figcaption>
+				<figcaption>
+				</figcaption>
+			</figure>
+			<figure>
+				<span id="img2"></span>
+				<figcaption>
+				</figcaption>
+				<figcaption>
+				</figcaption>
+			</figure>
+			<figure>
+				<span id="img3"></span>
+				<figcaption>
+				</figcaption>
+				<figcaption>
+				</figcaption>
+			</figure>
+			<figure>
+				<span id="img4"></span>
+				<figcaption>
+				</figcaption>
+				<figcaption>
+				</figcaption>
+			</figure>
+			<figure>
+				<span id="img5"></span>
+				<figcaption>
+				</figcaption>
+				<figcaption>
+				</figcaption>
+			</figure>
+			<figure>
+				<span id="img6"></span>
+				<figcaption>
+				</figcaption>
+				<figcaption>
+				</figcaption>
+			</figure>
+			<a href="#">
+			<div>
+				<img src="">
+				<article>
+					<time> </time>
+					<h2> </h2>
+					<p>
+
+					</p>
+				</article>
+				<img src="">
+				<article>
+					<time> </time>
+					<h2> </h2>
+					<p>
+
+					</p>
+				</article>
+			</div>
+			<a href="#">
+			<ul>
+				<li>
+					<a href="#">
+						<img src="">
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<img src="">
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<img src="">
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<img src="">
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<img src="">
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<img src="">
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<img src="">
+					</a>
+				</li>
+			</ul>
+		</main>
+		<footer class="wrap">
+			<div>
+			</div>
+			<div>
+			</div>
+			<div>
+			</div>
+			<p>
+			</p>
+		</footer>
+	</body>
+</html>
