84ad0826088963aef6a98839943449955a824a9c
Erik Balonier <erikibali@gmail.com>
2015-03-18 21:20:16 -0500

    finished main so far

--- index.php
+++ index.php
@@ -46,109 +46,136 @@
 			</section>					
 		</header>
 		<main class="wrap">
-			<h2>PORTFOLIO</h2>
-			<figure>
-				<span id="img1" class="img-animation"></span>
-				<figcaption>
-				</figcaption>
-				<figcaption>
-				</figcaption>
-			</figure>
-			<figure>
-				<span id="img2" class="img-animation"></span>
-				<figcaption>
-				</figcaption>
-				<figcaption>
-				</figcaption>
-			</figure>
-			<figure>
-				<span id="img3" class="img-animation"></span>
-				<figcaption>
-				</figcaption>
-				<figcaption>
-				</figcaption>
-			</figure>
-			<figure>
-				<span id="img4" class="img-animation"></span>
-				<figcaption>
-				</figcaption>
-				<figcaption>
-				</figcaption>
-			</figure>
-			<figure>
-				<span id="img5" class="img-animation"></span>
-				<figcaption>
-				</figcaption>
-				<figcaption>
-				</figcaption>
-			</figure>
-			<figure>
-				<span id="img6" class="img-animation"></span>
-				<figcaption>
-				</figcaption>
-				<figcaption>
-				</figcaption>
-			</figure>
-			<a href="#">View my beautiful <strong>PORTFOLIO</strong></a>
+			<section>
+				<h2>PORTFOLIO</h2>
+				<figure>
+					<span id="img1" class="img-animation"></span>
+					<figcaption>
+						LÖGREGLAN
+					</figcaption>
+					<figcaption>
+						www.logregla.is
+					</figcaption>
+				</figure>
+				<figure>
+					<span id="img2" class="img-animation"></span>
+					<figcaption>
+						SECRET SOLSTICE FESTIVAL
+					</figcaption>
+					<figcaption>
+						www.secretsolstice.is
+					</figcaption>
+				</figure>
+				<figure>
+					<span id="img3" class="img-animation"></span>
+					<figcaption>
+						PÁLL ÓSKAR
+					</figcaption>
+					<figcaption>
+						www.palloskar.is
+					</figcaption>
+				</figure>
+				<figure>
+					<span id="img4" class="img-animation"></span>
+					<figcaption>
+						22 HILL HOTEL
+					</figcaption>
+					<figcaption>
+						www.22hillhotel.is
+					</figcaption>
+				</figure>
+				<figure>
+					<span id="img5" class="img-animation"></span>
+					<figcaption>
+						NETGÍRÓ
+					</figcaption>
+					<figcaption>
+						www.netgiro.is
+					</figcaption>
+				</figure>
+				<figure>
+					<span id="img6" class="img-animation"></span>
+					<figcaption>
+						VESTURPORT
+					</figcaption>
+					<figcaption>
+						www.vesturport.com
+					</figcaption>
+				</figure>
+			</section>
+			<section>
+				<a href="#">View my beautiful <strong>PORTFOLIO</strong></a>
+			</section>
 			<section>
 				<h2>FOR MY LOVING STALKERS</h2>
 				<div>
-					<img src="">
-					<article>
-						<time> </time>
-						<h2> </h2>
-						<p>
-
-						</p>
-					</article>
-					<img src="">
-					<article>
-						<time> </time>
-						<h2> </h2>
-						<p>
-
-						</p>
-					</article>
+					<div>
+						<img src="http://www.gre.is/wp-content/uploads/2015/03/98917356-790x4031-375x250.jpg">
+						<article>
+							<time>March 4, 2015</time>
+							<h2>Thailand has me now </h2>
+							<p>
+								I set out to explore Bangkok with my better half to see if Thailand was actually a place where we could see ourselves living.
+							</p>
+						</article>
+					</div>
+					<div>
+						<img src="http://www.gre.is/wp-content/uploads/2015/03/krom_web_gre-1024x5141-375x250.jpg">
+						<article>
+							<time>March 4, 2015</time>
+							<h2>Building a brand new lifestyle website</h2>
+							<p>
+								I like people that have visions and goals and go after them full force. That’s why I didn’t hesitate when I got offered a share in a new online lifestyle magazine called Króm.
+							</p>
+						</article>
+					</div>
 				</div>
+				<a href="#">View my <strong>BLOG</strong></a>
 			</section>
-			<a href="#">View my <strong>BLOG</strong></a>
-			<ul>
-				<li>
-					<a href="#">
-						<img src="">
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						<img src="">
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						<img src="">
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						<img src="">
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						<img src="">
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						<img src="">
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						<img src="">
-					</a>
-				</li>
-			</ul>
+			<section>
+				<ul>
+					<li>
+						<a href="#">
+							<img src="http://scontent.cdninstagram.com/hphotos-xpa1/t51.2885-15/s306x306/e15/924760_1570352069891888_1612477989_n.jpg">
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<img src="http://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s306x306/e15/11055861_1076380562377419_1704138908_n.jpg">
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<img src="http://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s306x306/e15/11049265_566895413452205_66415878_n.jpg">
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<img src="http://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s306x306/e15/11032978_339873142874010_1897642526_n.jpg">
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<img src="http://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s306x306/e15/10838499_924944977538010_1150631732_n.jpg">
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<img src="http://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s306x306/e15/11055967_869589413107929_633624126_n.jpg">
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<img src="http://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s306x306/e15/11007974_1423517671278770_1824586534_n.jpg">
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<img src="http://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s306x306/e15/11055870_1078754442151745_303631007_n.jpg">
+						</a>
+					</li>
+				</ul>
+			</section>
 		</main>
 		<footer>
 			<section class="wrap">
